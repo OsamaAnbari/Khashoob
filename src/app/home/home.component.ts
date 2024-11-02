2af2dea72ce0341834ada7fd09bcaf4bfcd334ac
Osama Anbari <oanbari@nbsventure.com>
2024-11-02 21:59:20 +0300

    image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,17 +9,17 @@ export class HomeComponent {
   products = [
     { 
       name: 'Product 1', 
-      image: 'assets/images/product1.jpg', 
+      image: '', 
       description: 'This is a description of Product 1.' 
     },
     { 
       name: 'Product 2', 
-      image: 'assets/images/product2.jpg', 
+      image: '', 
       description: 'This is a description of Product 2.' 
     },
     { 
       name: 'Product 3', 
-      image: 'assets/images/product3.jpg', 
+      image: '', 
       description: 'This is a description of Product 3.' 
     },
   ]
