d141e4acb7c3058024e9ad37bee8c0e089712eaa
Osama Anbari <oanbari@nbsventure.com>
2024-11-11 21:24:26 +0300

    styles

--- src/app/Layout/navbar/navbar.component.ts
+++ src/app/Layout/navbar/navbar.component.ts
@@ -7,9 +7,18 @@ import { Location } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
   constructor(private router: Router, private route: ActivatedRoute, private location: Location) {}
+
+  logo: string = 'Images/logo.png';
+
+  ngOnInit(): void { 
+    this.route.paramMap.subscribe(params => { 
+      const lang = params.get('lang');
+      this.logo = lang === 'ar' ? 'Images/logo.png' : 'Images/logo-EN.png';
+    });
+  }
 
   navigateTo(route: string) { 
     const currentUrl = this.router.url.split('/')[1]; 
@@ -22,6 +31,14 @@ export class NavbarComponent {
     url = url.replace('/ar', lang); // Change 'en' to 'ar' 
     document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr'
     document.title = lang === 'ar' ? 'خشوب' : 'Khashoob';
+    this.logo = lang === 'ar' ? 'Images/logo.png' : 'Images/logo-EN.png';
     this.router.navigateByUrl(url);
+  }
+
+  scrollToTarget(target: string) { 
+    const targetElement = document.getElementById(target); 
+    if (targetElement) { 
+      targetElement.scrollIntoView({ behavior: 'smooth' }); 
+    } 
   }
 }
